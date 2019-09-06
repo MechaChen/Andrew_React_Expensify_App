96724b8bcbef05b34175fdded5a59c517eb524c3
Benson Chen <tumlivein@gmail.com>
2019-09-06 18:01:30 +0800

    76. Setting Up Budget App

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "reset.css/reset.css";
+import "./styles/style.scss";
+
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("app"));
