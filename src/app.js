dc2cfd9aea220b063f9f3d2f070e16344649fdff
Benson <tumlivein@gmail.com>
2019-09-07 13:15:14 +0800

    80. Organizing Our Routes: making Routing a Single file

--- src/app.js
+++ src/app.js
@@ -1,57 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import AppRouter from "./routers/AppRouter";
 import "reset.css/reset.css";
 import "./styles/style.scss";
 
-const ExpenseDashboardPage = () => (
-  <div>This is from my dashboard component</div>
-);
-
-const AddExpensePage = () => <div>This is from my add expense component</div>;
-
-const EditExpensePage = () => <div>This is from my edit expense component</div>;
-
-const HelpPage = () => <div>This is from my help component</div>;
-
-const NotFoundPage = () => (
-  <div>
-    404 - <Link to="/">Go Home</Link>
-  </div>
-);
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>
-      Dashboard
-    </NavLink>
-    <NavLink to="/create" activeClassName="is-active">
-      Create Expense
-    </NavLink>
-    <NavLink to="/edit" activeClassName="is-active">
-      Edit Expense
-    </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      Help
-    </NavLink>
-  </header>
-);
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Header />
-      {/* Switch will find the 1st match route */}
-      <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById("app"));
+ReactDOM.render(<AppRouter />, document.getElementById("app"));
