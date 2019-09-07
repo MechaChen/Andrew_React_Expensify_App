32d5a9ee859330b75421a4240f045fa2634307df
Benson <tumlivein@gmail.com>
2019-09-07 13:59:10 +0800

    Challenge

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const EditExpensePage = () => <div>This is edit component</div>;
+
+export default EditExpensePage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,45 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import Header from "../components/Header";
+import ExpenseDashboardPage from "../components/ExpenseDashboardPage";
+import AddExpensePage from "../components/AddExpensePage";
+import EditExpensePage from "../components/EditExpensePage";
+import HelpPage from "../components/HelpPage";
+import NotFoundPage from "../components/NotFoundPage";
 
-const ExpenseDashboardPage = () => (
-  <div>This is from my dashboard component</div>
-);
-
-const AddExpensePage = () => <div>This is from my add expense component</div>;
-
-const EditExpensePage = () => <div>This is from my edit expense component</div>;
-
-const HelpPage = () => <div>This is from my help component</div>;
-
-const NotFoundPage = () => (
-  <div>
-    404 - <Link to="/">Go Home</Link>
-  </div>
-);
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>
-      Dashboard
-    </NavLink>
-    <NavLink to="/create" activeClassName="is-active">
-      Create Expense
-    </NavLink>
-    <NavLink to="/edit" activeClassName="is-active">
-      Edit Expense
-    </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      Help
-    </NavLink>
-  </header>
-);
+// Create 6 new files for the 6 components
+// setup imports, component, default export
+// import into AppRouter so they can be used
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Header />
-      {/* Switch will find the 1st match route */}
       <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true} />
         <Route path="/create" component={AddExpensePage} />
