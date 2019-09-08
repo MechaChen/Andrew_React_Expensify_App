a6cf68955aefa1ed10fece6718d87a255f464743
Benson <tumlivein@gmail.com>
2019-09-08 23:25:57 +0800

    Challenge

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -20,11 +20,29 @@ const expenseReducer = (state = expenseReducerDefaultState, action) => {
   }
 };
 
+// Filter Reducer
+// text => '', sortBy => 'date', startDate => undefined, endDate => undefined
+
+const filtersReducerDefaultState = {
+  text: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
 // Store creation
 
 const store = createStore(
   combineReducers({
-    expense: expenseReducer
+    expense: expenseReducer,
+    filters: filtersReducer
   })
 );
 
