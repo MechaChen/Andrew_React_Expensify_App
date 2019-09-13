cf8e8c309050aac3e6de193924431c69f81e8d1d
Benson <tumlivein@gmail.com>
2019-09-13 20:38:55 +0800

    105. Creating Expense Add/Edit Form: Add Expense Form

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default class ExpenseForm extends React.Component {
+  state = {
+    description: ""
+  };
+  onDescriptionChange = e => {
+    const description = e.target.value;
+    this.setState(() => ({ description }));
+  };
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="Description"
+            autoFocus
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+          />
+          <input type="number" placeholder="Amount" />
+          <textarea placeholder="Add a note for your expense(optional)"></textarea>
+        </form>
+      </div>
+    );
+  }
+}
