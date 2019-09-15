cd7ee23aeaf46dc15547cafe65c0d1239de7ebd0
Benson <tumlivein@gmail.com>
2019-09-15 21:54:19 +0800

    Challenge: Edit Expense Test

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,3 +7,16 @@ test("should setup remove expense action object", () => {
     id: "123abc"
   });
 });
+
+// Setup test case
+// Call editExpense { note: 'New note value' }
+// Make an assertion
+
+test("should setup edit expense aciton object", () => {
+  const action = editExpense("123abc", { note: "New note value" });
+  expect(action).toEqual({
+    type: "EDIT_EXPENSE",
+    id: "123abc",
+    updates: { note: "New note value" }
+  });
+});
