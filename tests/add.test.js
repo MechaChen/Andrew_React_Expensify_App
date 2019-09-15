02ff2f856b73d3f37ac4552e672b4b687b3a84d1
Benson <tumlivein@gmail.com>
2019-09-15 20:50:57 +0800

    112. Setting up Jest: Jest Assertion

--- tests/add.test.js
+++ tests/add.test.js
@@ -2,8 +2,5 @@ const add = (a, b) => a + b + 1;
 
 test("should add two number.", () => {
   const result = add(3, 4);
-
-  if (result !== 7) {
-    throw new Error(`You added 4 and 3. The result is ${result}. Expect 7`);
-  }
+  expect(result).toBe(7);
 });
