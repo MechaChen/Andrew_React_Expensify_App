c735866893e28b92e0cd7bd7925a5934df6273b1
Benson <tumlivein@gmail.com>
2019-09-15 20:47:09 +0800

    112. Setting up Jest: 1st Test

---
+++ tests/add.test.js
@@ -0,0 +1,9 @@
+const add = (a, b) => a + b;
+
+test("should add two number.", () => {
+  const result = add(3, 4);
+
+  if (result !== 7) {
+    throw new Error(`You added 4 and 3. The result is ${result}. Expect 7`);
+  }
+});
