e6adfbc4ac1b2152c2d8cfa28eb4fff77b20d47a
Benson Chen <tumlivein@gmail.com>
2019-09-17 18:03:03 +0800

    117. Testing Expenses Reducer

---
+++ src/tests/reducers/expeses.test.js
@@ -0,0 +1,13 @@
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+
+test('should set default state', () => {
+});
+
+test('should remove expense by id', () => {
+
+});
+
+test('should not remove expense if id not found', () => {
+
+});
