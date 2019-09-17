b25627e74c5abdc1886ddaf658ae36270fd04b96
Benson Chen <tumlivein@gmail.com>
2019-09-17 17:28:26 +0800

    116. Testing Filters Reducer

---
+++ src/tests/reducers/filters.test.js
@@ -0,0 +1,28 @@
+import filtersReducer from '../../reducers/filters';
+import moment from 'moment';
+
+test('should setup default filter value', () => {
+  const state = filtersReducer(undefined, { type: '@@INIT' });
+  expect(state).toEqual({
+    text: '',
+    sortBy: 'date',
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month'),
+  });
+});
+
+test('should set sortBy to amount', () => {
+  const state = filtersReducer(undefined, { type: 'SORT_BY_AMOUNT' });
+  expect(state.sortBy).toBe('amount');
+});
+
+test('should set sortBy to date', () => {
+  const currentState = {
+    text: '',
+    sortBy: 'amount',
+    startDate: undefined,
+    endDate: undefined,
+  };
+  const state = filtersReducer(currentState, { type: 'SORT_BY_DATE' });
+  expect(state.sortBy).toBe('date');
+});
