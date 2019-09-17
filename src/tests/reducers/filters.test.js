927b8afbb289f15b030c394d95b52db87f549f6d
Benson Chen <tumlivein@gmail.com>
2019-09-17 17:43:53 +0800

    Challenge

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -26,3 +26,35 @@ test('should set sortBy to date', () => {
   const state = filtersReducer(currentState, { type: 'SORT_BY_DATE' });
   expect(state.sortBy).toBe('date');
 });
+
+// should set text filter
+test('should set text filter', () => {
+  const action = {
+    type: 'SET_TEXT_FILTER',
+    text: 'hello',
+  };
+  const state = filtersReducer(undefined, action);
+  expect(state.text).toBe('hello');
+});
+
+// should set startDate filter
+test('should set startDate filter', () => {
+  const startDate = moment().subtract(2, 'month');
+  const action = {
+    type: 'SET_START_DATE',
+    startDate
+  };
+  const state = filtersReducer(undefined, action);
+  expect(state.startDate).toBe(startDate);
+});
+
+// should set endDate filter
+test('should set endDate filter', () => {
+  const endDate = moment().add(1, 'month');
+  const action = {
+    type: 'SET_END_DATE',
+    endDate
+  };
+  const state = filtersReducer(undefined, action);
+  expect(state.endDate).toBe(endDate);
+});
