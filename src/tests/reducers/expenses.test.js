e9faebd87720a0a79d41790679cd1f8117b1691c
Benson <tumlivein@gmail.com>
2019-09-17 23:48:54 +0800

    117. Testing Expenses Reducer

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,30 @@
+// should set default state
+// should remove expesne by id
+// should not remove expenses if id not found
+
+import expensesReducer from '../../reducers/expenses';
+import moment from 'moment';
+import expenses from '../fixtures/expenses';
+
+test('should set default state', () => {
+  const state = expensesReducer(undefined, { type: '@@INIT' });
+  expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: expenses[1].id
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('should not remove expeneses if id not found', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: '-1'
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
