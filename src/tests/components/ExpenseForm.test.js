6de48c5db8f952a3c9bbe3276c9a37bb8d254e16
Benson <tumlivein@gmail.com>
2019-09-18 22:44:10 +0800

    Challenge

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ExpenseForm from '../../components/ExpenseForm';
+import expenses from '../fixtures/expenses';
 
 test('should render ExpenseForm correctly', () => {
   const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+// should render ExpenseForm with expense data
+test('should render ExpenseForm with a expense', () => {
+  const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
   expect(wrapper).toMatchSnapshot();
 });
