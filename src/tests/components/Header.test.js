d07e318eb122c3e943a97ef76bc3895bf4ae5472
Benson Chen <tumlivein@gmail.com>
2019-09-18 15:53:01 +0800

    119. Enzyme: Enzyme with .toMatchSnapshot()

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -4,8 +4,9 @@ import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
-  expect(wrapper.find('h1').text()).toBe('Expensify');
+  expect(wrapper).toMatchSnapshot();
 
+  // expect(wrapper.find('h1').text()).toBe('Expensify');
   // const renderer = new ReactShallowRenderer();
   // renderer.render(<Header />);
   // expect(renderer.getRenderOutput()).toMatchSnapshot();
