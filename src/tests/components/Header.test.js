7a76f78a192c9851046ef11a90f7f1bb07fda85c
Benson Chen <tumlivein@gmail.com>
2019-09-18 15:56:45 +0800

    119. Enzyme: use enzyme-to-json to remove unneccesary enzyme snapshot

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
-  expect(wrapper).toMatchSnapshot();
+  expect(toJSON(wrapper)).toMatchSnapshot();
 
   // expect(wrapper.find('h1').text()).toBe('Expensify');
   // const renderer = new ReactShallowRenderer();
