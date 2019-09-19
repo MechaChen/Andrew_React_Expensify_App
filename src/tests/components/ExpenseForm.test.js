bded2b11d55c0c53986bf0d6cb797fcc6c284b51
Benson Chen <tumlivein@gmail.com>
2019-09-19 11:56:16 +0800

    122. Testing User Interaction: Simulate description Input Change Event

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -16,9 +16,24 @@ test('should render ExpenseForm with a expense', () => {
 test('should render error for invalid form submission', () => {
   const wrapper = shallow(<ExpenseForm />);
   expect(wrapper).toMatchSnapshot();
-  wrapper.find('form').simulate('submit', {
-    preventDefault: () => {}
-  });
+  wrapper
+    .find('form')
+    .simulate('submit', {
+      preventDefault: () => {}
+    });
   expect(wrapper.state('error').length).toBeGreaterThan(0);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('should set description on input change', () => {
+  const value = 'New description';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper
+    .find('input')
+    .at(0)
+    .simulate('change', {
+      target: { value }
+    });
+  expect(wrapper.state('description')).toBe(value);
+});
+
