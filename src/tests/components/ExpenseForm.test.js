390dac7342304f7a49e9f5d9d32fe08e2bcb9f24
Benson Chen <tumlivein@gmail.com>
2019-09-19 14:18:39 +0800

    123. Test Spies:  Submit Form with Parameters

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -81,6 +81,19 @@ test('should not set amount if invalid input', () => {
 
 test('should call onSubmit prop for valid form submission', () => {
   const onSubmitSpy = jest.fn();
-  onSubmitSpy('Benson', 'Taichung');
-  expect(onSubmitSpy).toHaveBeenCalledWith('Benson', 'Taichung');
+  const wrapper = shallow(
+    <ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy}/>
+  );
+  wrapper
+    .find('form')
+    .simulate('submit', {
+      preventDefault() {}
+    });
+  expect(wrapper.state('error')).toBe(null);
+  expect(onSubmitSpy).toHaveBeenLastCalledWith({
+    description: expenses[0].description,
+    note: expenses[0].note,
+    amount: expenses[0].amount,
+    createdAt: expenses[0].createdAt,
+  });
 });
