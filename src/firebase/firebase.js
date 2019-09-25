b622aadf2c200e5e48e7831828cf136b442e8d60
Benson <tumlivein@gmail.com>
2019-09-25 23:21:31 +0800

    146. Removing Data from Firebase: Challenge: Removing isSingle

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -17,46 +17,30 @@ const database = firebase.database();
 
 // in rules, set ".read" and ".write" to false
 
+// database
+//   .ref()
+//   .set({
+//     name: 'Benson Chen',
+//     age: 25,
+//     isSingle: true,
+//     location: {
+//       city: 'Taichung',
+//       country: 'Taiwan',
+//     }
+//   })
+//   .then(() => {
+//     console.log('Data is saved');
+//   })
+//   .catch((e) => {
+//     console.log('This faild.', e);
+//   });
+
 database
-  .ref()
-  .set({
-    name: 'Benson Chen',
-    age: 25,
-    isSingle: true,
-    location: {
-      city: 'Taichung',
-      country: 'Taiwan',
-    }
-  })
+  .ref('isSingle')
+  .remove()
   .then(() => {
-    console.log('Data is saved');
+    console.log('Data was removed');
   })
   .catch((e) => {
-    console.log('This faild.', e);
-  });
-
-
-// database
-//   .ref('age')
-//   .set(27);
-// database
-//   .ref('location/city')
-//   .set('Taipei');
-
-// setup then and catch
-// make sure catch actually works
-// switch rules to be open
-// make sure that then runs
-
-database
-  .ref('attributes')
-  .set({
-    height: 175,
-    weight: 75,
-  })
-  .then(() => {
-    console.log('add attributes successfully!');
-  })
-  .catch((error) => {
-    console.log(error);
+    console.log('Did not remove data', e)
   });
