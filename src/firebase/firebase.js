46d81249473081dd2ecbde424a196c55b01fba82
Benson <tumlivein@gmail.com>
2019-09-25 23:24:01 +0800

    146. Removing Data from Firebase: use Set to Remove Data

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -17,30 +17,34 @@ const database = firebase.database();
 
 // in rules, set ".read" and ".write" to false
 
-// database
-//   .ref()
-//   .set({
-//     name: 'Benson Chen',
-//     age: 25,
-//     isSingle: true,
-//     location: {
-//       city: 'Taichung',
-//       country: 'Taiwan',
-//     }
-//   })
-//   .then(() => {
-//     console.log('Data is saved');
-//   })
-//   .catch((e) => {
-//     console.log('This faild.', e);
-//   });
+database
+  .ref()
+  .set({
+    name: 'Benson Chen',
+    age: 25,
+    isSingle: true,
+    location: {
+      city: 'Taichung',
+      country: 'Taiwan',
+    }
+  })
+  .then(() => {
+    console.log('Data is saved');
+  })
+  .catch((e) => {
+    console.log('This faild.', e);
+  });
 
 database
   .ref('isSingle')
-  .remove()
-  .then(() => {
-    console.log('Data was removed');
-  })
-  .catch((e) => {
-    console.log('Did not remove data', e)
-  });
+  .set(null);
+
+// database
+//   .ref()
+//   .remove()
+//   .then(() => {
+//     console.log('Data was removed');
+//   })
+//   .catch((e) => {
+//     console.log('Did not remove data', e)
+//   });
