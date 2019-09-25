2c6ebf381e05401e5995ded48824589d8820c72c
Benson <tumlivein@gmail.com>
2019-09-25 22:08:20 +0800

    144. ES6 Promises: Reject & Catch

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,16 +1,21 @@
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve({
-      name: 'Benson',
-      age: 25,
-    });
+    // resolve({
+    //   name: 'Benson',
+    //   age: 25,
+    // });
+    reject('Something went wrong!');
   }, 5000);
 });
 
 console.log('before');
 
-promise.then((data) => {
-  console.log('1', data);
-});
+promise
+  .then((data) => {
+    console.log('1', data);
+  })
+  .catch((error) => {
+    console.log('error:', error);
+  });
 
 console.log('after');
